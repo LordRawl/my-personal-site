36376366a97cbf3c7b70fdfc222cf63dd05a8f5e
Ivan <lordrawl@mail.ru>
2026-08-27 13:28:00 +0300

    fix(metrika): use Yandex official ym async loader stub - prevents 'window.ym is not a function'

--- app/app.vue
+++ app/app.vue
@@ -60,19 +60,30 @@ onMounted(() => {
   initTheme()
   initReveal()
 
-  const script = document.createElement('script')
-  script.async = true
-  script.src = 'https://mc.yandex.ru/metrika/tag.js'
-  document.head.appendChild(script)
-  script.onload = () => {
-    ;(window as any).ym(112000128, 'init', {
-      defer: true,
-      clickmap: true,
-      trackLinks: true,
-      accurateTrackBounce: true,
-      webvisor: true,
-    })
-  }
+  ;(function (m, e, t, r, i, k, a) {
+    m[i] =
+      m[i] ||
+      function () {
+        ;(m[i].a = m[i].a || []).push(arguments)
+      }
+    m[i].l = 1 * new Date()
+    for (let j = 0; j < document.scripts.length; j++) {
+      if (document.scripts[j].src === r) return
+    }
+    k = e.createElement(t)
+    a = e.getElementsByTagName(t)[0]
+    k.async = 1
+    k.src = r
+    a.parentNode.insertBefore(k, a)
+  })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym')
+
+  ym(112000128, 'init', {
+    defer: true,
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: true,
+  })
 })
 </script>
 
