5a2f83d0e66bb1108bb7680d94e075b5a157e848
Ivan <lordrawl@mail.ru>
2026-08-28 15:00:01 +0300

    fix(metrika): load counter from SSR head (useHead) instead of onMounted - reliable init independent of Vue hydration

--- app/app.vue
+++ app/app.vue
@@ -47,6 +47,11 @@ useHead({
         ...(siteUrl ? { url: siteUrl } : {}),
       }),
     },
+    {
+      key: 'yandex-metrika',
+      type: 'text/javascript',
+      innerHTML: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return}}k=e.createElement(t);a=e.getElementsByTagName(t)[0];k.async=1;k.src=r;a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js','ym');ym(112000128,'init',{defer:true,clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});`,
+    },
   ],
   noscript: [
     {
@@ -59,31 +64,6 @@ useHead({
 onMounted(() => {
   initTheme()
   initReveal()
-
-  ;(function (m, e, t, r, i, k, a) {
-    m[i] =
-      m[i] ||
-      function () {
-        ;(m[i].a = m[i].a || []).push(arguments)
-      }
-    m[i].l = 1 * new Date()
-    for (let j = 0; j < document.scripts.length; j++) {
-      if (document.scripts[j].src === r) return
-    }
-    k = e.createElement(t)
-    a = e.getElementsByTagName(t)[0]
-    k.async = 1
-    k.src = r
-    a.parentNode.insertBefore(k, a)
-  })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym')
-
-  ym(112000128, 'init', {
-    defer: true,
-    clickmap: true,
-    trackLinks: true,
-    accurateTrackBounce: true,
-    webvisor: true,
-  })
 })
 </script>
 
